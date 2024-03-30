a5eda4919c0f0952f4daefa170b6c2b1228c0109
Helias <stefanoborzi32@gmail.com>
2024-03-30 21:57:56 +0000

    Deploying to gh-pages from @ UNICT-Quality-Development/git-catalogue@47bffa440d9f71d8116c7662350d8cabfc667432 🚀

--- src/app/catalogue/catalogue.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-catalogue',
-  templateUrl: './catalogue.component.html',
-})
-export class CatalogueComponent {}
--- src/app/pipes/emoji-fixup.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EmojiFixupPipe } from './emoji-fixup.pipe';
-
-describe('EmojiFixupPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EmojiFixupPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
